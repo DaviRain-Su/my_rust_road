4c951e37962e8a541c15d6a4d2d8ab0523921555
DaviRain-Su <davirain.yin@gmail.com>
2020-08-07 21:44:57 +0800

    compilte chapter3

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,16 +1,17 @@
 extern crate chapter3;
 fn main() {
-    // chapter3::Davirain::str_compose();
+
+    chapter3::davirain::str_compose();
     
-    // chapter3::Davirain::compare_size();
+    // chapter3::davirain::compare_size();
 
     // let mut arr = [1, 2, 3, 4, 5];
     // println!("before arr: {:?}", arr);
-    // chapter3::Davirain::reset(&mut arr);
+    // chapter3::davirain::reset(&mut arr);
     
-    // chapter3::Davirain::zero_size();
+    // chapter3::davirain::zero_size();
 
-    // chapter3::Davirain::zero_size_loop();
+    // chapter3::davirain::zero_size_loop();
     // chapter3::davirain::bottom_type();
     // chapter3::davirain::infer_demo();
     // chapter3::davirain::infer_generics();
@@ -30,5 +31,59 @@ fn main() {
     // chapter3::davirain::manual_deref();
     // chapter3::davirain::fqsfd();
     // chapter3::davirain::from_into();
-    chapter3::davirain::trait_limit();
+    // chapter3::davirain::trait_limit();
+    // trait_object_dyn();
+    // use_and_then();
+    // use_map();
+    // chapter3::davirain::trait_special();
+}
+
+
+fn trait_object_dyn() {
+    trait Shape {
+        fn area(&self) -> f64;
+    }
+    trait Round {
+        fn get_radius(&self) -> f64;
+    }
+    #[derive(Debug)]
+    struct Circle {
+        radius: f64,
+    }
+
+    impl Round for Circle {
+        // self : &Circle
+        fn get_radius(&self) -> f64 {
+            self.radius
+        }
+    }
+
+    impl Shape for dyn Round { // impl trait to dyn trait 
+        fn area(&self) -> f64 { // self : &Round
+            std::f64::consts::PI * self.get_radius() * self.get_radius()
+        }
+    }
+
+    let c = Circle { radius: 2f64 };
+    println!("The Circle is {:?}", c);
+    // c.area();
+    let b = Box::new(Circle { radius: 4f64}) as Box<dyn Round>;
+    let p = b.area();
+    println!("The area is {}", p);
+}
+
+fn use_and_then() {
+    fn sq(x: u32) -> Option<u32> {
+        Some(x * x)
+    }
+
+    println!("{:?}", Some(2).and_then(sq).and_then(sq));
+    let x = Some(2).and_then(sq).and_then(sq).unwrap();
+    println!("the value is {}", x);
+}
+
+fn use_map() {
+    let maybe_some_string = Some(String::from("hello, world"));
+    let maybe_some_len = maybe_some_string.map(|s| s.len()).unwrap();
+    println!("the maybe_some_string len is {}", maybe_some_len);
 }
