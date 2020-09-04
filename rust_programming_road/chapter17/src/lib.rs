b8455930b260b2efb209fa7235ad3f7dc6189ff4
DaviRain-Su <davirain.yin@gmail.com>
2020-09-04 23:30:29 +0800

    ...

--- rust_programming_road/chapter17/src/lib.rs
+++ rust_programming_road/chapter17/src/lib.rs
@@ -345,7 +345,7 @@ fn impl_trat() {
     // let screen = Screen {
         // components: vec![Box::new(selectbox), Box::new(button)],
     // };
-    
+
     let mut  screen  = Screen::new();
     
     screen.add(Box::new(selectbox));
@@ -358,4 +358,293 @@ fn impl_trat() {
     screen.add(str);
     screen.run();
 }
- 
+
+/// # triat 对象会执行动态派发
+/// 
+/// 对于泛型
+/// 
+/// Rust编译器会在泛型使用trait约束时执行单态化：编译器会为每一个具体类型生成
+/// 对应泛型函数和泛型方法的非泛型实现，并使用这些具体地类型来替代泛型参数。
+/// 
+/// 通过单态化生成的代码会执行静态派发，
+/// 这意味着编译器能够在编译器过程中确定你调用的具体方法。
+/// 
+/// 对于动态派发
+/// 
+/// 动态派发下的编译器无法在编译过程中确定你调用的究竟是哪一个方法。
+/// 在进行动态派发的场景中，编译器会生成一些额外的代码以便在运行时找出我们希望调用的方法。
+/// 
+/// 对于动态派发Rust使用triat 对象来实现。
+/// 
+/// 因为编译器无法知道所有能够用于tirat对象的具体类型，所以它无法再
+/// 编译时确定需要调用哪个类型的具体方法。rust会在运行时通过tirat对象内部的指针去定位
+/// 具体调用哪个方法。
+/// 
+/// 动态派发会阻止编译器内联代码，进而使得部分优化操作无法进行。
+/// 
+/// ## trait对象必须保证对象安全
+/// 
+/// 
+/// tirat对象的使用需要注意的是，
+/// 
+/// trait对象， **只能把满足对象安全的trait转换为trait对象。**
+/// 
+/// 如果一个trait中定义的所有方法满足下面两条规则，那么这个triat就是对象安全的：
+/// - 方法的返回类型不是Self。
+/// - 方法中不包含任何泛型参数
+/// 
+/// 
+/// 解释第一条：
+/// 
+/// 关键字Self是一个别名，它指向了实现当前trait或方法的具体类型。
+/// triat对象必须是对象安全的，因为Rust无法再我们使用trit对象时确定实现这个triat的具体
+/// 类型究竟是什么。由于trait对象忘记 Self的具体类型，所以编译器无法再trait方法返回Self
+/// 时使用原来的具体类型。
+/// 
+/// 解释第二条：
+/// 
+/// 对于trait方法中的泛型参数而言，我们会在使用时将具体类型填入泛型所处的位置，这些具体
+/// 类型会被视作当前类型的一部分。由于trait对象忘记了类型信息，所以我们无法确定被填入泛型参数处
+/// 的类型究竟是哪一个。
+/// 
+/// 
+/// 标准库中的Clone trait就是一个不符合对象安全的例子。
+/// ```
+/// pub trait Clone {
+///     fn clone(&self) -> Self;
+/// }
+/// ```
+/// 
+/// 编译器会在你使用trait对象时，指出违反了对象安全规则的地方。
+/// 
+pub fn triat_object_safe() {
+    pub struct Screen {
+        // pub components : Vec<Box<dyn Clone>>,
+        //the trait `std::clone::Clone` cannot be made into an object
+        // the trait cannot be made into an object because it requires `Self: Sized`
+    }
+    // 错误信息表明我们不能按照这种方式将Clone trait 用作trait对象。
+}
+
+/// # 实现一种面向对象的设计模式
+/// 
+/// 
+/// - 状态模式的实现：
+/// 
+/// 状态模式是一种面向对象的设计模式，
+/// 关键点是，
+/// 一个值拥有的内部状态由数个状态对象表达而成，而值的行为随着内部状态的改变而改变。
+/// 这种设计模式会通过状态对象累共享功能：在Rust中使用了结构体与trait而不是对象与集成类实现这一特性。
+/// 
+/// 每个状态对象都会负责自己的行为并掌握自己转换为其他状态的时机。而持有状态对象的值则对
+/// 状态的不同行为和状态转换的时机一无所知。
+/// 
+/// 使用状态模式意味着在业务需求发生变化时，我们不需要修改持有状态对象的值，或者使用这个值的代码。
+/// 我们只需要更新对象的代码或增加一些新的状态对象，就可以改变程序的运转规则。
+/// 
+/// 
+/// # Demo， 采用增量模式的开发过程来实现一个用于发布博客的工作流。
+/// 
+/// 1. 在新建博客文章时生成一份空白的草稿文档。（草稿状态）
+/// 
+/// 2. 在草稿撰写完毕后，请求对这篇草稿状态的文章进行审批。（草稿状态 --> 审批状态）
+/// 
+/// 3. 在文章通过审批后正式多对外发布。 （审批状态 --> 发布状态）
+/// 
+/// 4. 仅返回并打印成功发布后的文章，而不能意外地发布没有通过审批的文章。(规定)
+/// 
+/// 除了上面描述的流程，任何其他对文章的修改行为都应当时无效的。
+/// 例如， 假设有人试图跳过审批过程来直接发布草稿状态的文章，那么我们的程序应当
+/// 阻止这一行为并保存文章的草稿状态。
+/// 
+/// 
+/// 用户在使用这个Blog库的时候，涉及到数据类型只有Post类型，这个类型会采用状态模式，
+/// 它持有的值会是三种不同的文章的状态对象中的一个：草稿、等待审批或发布。Post类型
+/// 会在内部管理状态与状态之间的变化过程。虽然状态变化的行为会在用户调用Post实例的对应
+/// 方法是发生，但用户并不需要直接对这一过程进行管理。这同样意味着用户不会因为状态
+/// 而出错，比如在审批未完成前发布文章。
+/// 
+/// ## 定义Post并新建一个处于草稿状态下的新实例
+/// 
+/// 定义一个存储内容的公共结构体Post
+/// 
+/// ## 存储文章的内容
+/// 
+/// add_text()
+/// 
+/// ## 确保草稿的可读内容为空
+///  
+/// 
+/// 即便用户调用add_text方法为文章添加了一些内容，但只要文章处于草稿状态。
+/// 我们就需要在用户调用conext方法时范湖一个空的字符串
+/// 
+/// ## 请求审批文章并改变其状态
+/// 
+/// 请求审批文章的功能会将文章的状态从Draft编为PendingReview
+/// 
+/// ## 添加approve 方法来改变content的行为
+/// 
+/// approve方法与request_review方法类似，他会执行状态的审批流程，并将state设置
+/// 为当前状态审批后返回的值。
+/// 
+///  
+pub struct Post {
+    state: Option<Box<dyn State>>, //state用于存放状态，这里需要去做状态的转换
+    // 不使用Pub, 是要控制用户访问content的权限，控制用户访问字段的数据时的具体行为
+    content: String, // 存放文章的内容
+}
+impl Post {
+    pub fn new() -> Self {
+        Self {
+            // 这里将状态设置为了Draft，任何创建出来的Post实例
+            // 都会从草稿状态开始。
+            // 因为Post的state字段是私有的
+            // , 所以用户无法采用其他状态来创建Post。
+            state: Some(Box::new( Draft {} )), // 私有的
+            content: String::new(),           
+        }
+    }
+
+    //该方法不依赖与文章当前所处的状态，所以他不是状态模式的一部分。
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn request_review(&mut self) {
+        // 做状态的转换
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take(){
+            self.state = Some(s.approve());
+        }
+    }
+
+    // 更新Post的content方法，在该方法中委托调用State的content方法
+    // 
+    // 因为我们希望所有的规则在State相关结构体的内部实现，所以我们会调用
+    // state值的content方法，并将Post实例本身作为参数传入，最后将这个方法返回的值作为结果
+    // 
+    // 这里代用了Option的as_ref方法，因为我们需要的只是Option中的引用，而不是他的所有权。
+    // 由于state的乐行是Option<Box<dyn State>>, 所以我们会在调用as_ref时得到Option<&Box<State>>
+    // 如果这段代码中没有调用as_ref， 那么就会导致编译时的错误，因为我们不能将state从函数参数
+    // 的借用&self中移出
+    //
+    // 接着调用了unwarp方法，这是因为Post的具体示例中保证了方法调用结束时state总是会有一个
+    // 有效的Some值，所以可以 确定的调用unwrap不会发生panic. 
+    //
+    //
+    // 又调用了&Box<dyn State>的content方法，由于解引用会一次作用于&, Box, 所以
+    // 我们最终调用的content方法来自实现了 State trait的具体类型。
+    // 这意味着我们需要在 State trait 的定义中添加content方法，并在这个方法的实现中
+    // 基于当前状态决定究竟返回那些内容。
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
+    }
+}
+
+/// 接口，不同的状态需要去实现这个State trait
+/// State trait 定义了所有文章状态共享的行为，
+/// Draft,
+/// PendingReview,
+/// Published
+/// 都会实现 State trait
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;    
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+
+    // 这个方法需要加上生命周期，因为返回的是Post结构中的content的&str，
+    // 这个方法的实现爱内需要接受post的引用作为参数，并返回post中某一部分的引用
+    // 作为结果，因此该方法中返回值的生命周期应该与post参数的生命周期相关。
+    fn content<'a>(&self, _post: &'a Post) -> &'a str{
+        ""
+    }
+}
+
+/// 草稿状态
+struct Draft {}
+
+/// 为草稿状态实现这个 State，这个tarit是状态转换的
+impl State for Draft {
+    // 消耗传入的草稿状态，变为审校状态
+    // self : Box<Self> 作为第一个参数，这个语法意味着该方法只能被包裹着当前类型的
+    // Box实例调用，他会在调用过程中获取Box<Self>的所有权并使旧的状态失效，从而将Post
+    // 的状态值转换为一个新的状态。
+    //
+    // 为了消耗旧的状态，, request_review方法需要获取状态值的所有权，这也正是Post的state
+    // 字段引入Option的原因： Rust不允许结构体中出现未被填充的值。
+    // 
+    // 可以通过Option<T>的take方法来取出state字段的Some值，并在原来的位置留下一个None。
+    // 这样做使我们能够将state的值从Post中移出来， 而不单单只是借用它.
+    //
+    // 这样做也确保了Post无法再我们完成状态转换后再次使用旧的state值. 
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    // 这里Draft也实现了content只不过这里是默认的实现方式
+}
+
+struct PendingReview{}
+
+impl State for PendingReview {
+    // 对于一篇已经处在PendingRevirw状态下的文章，发起审批请求并不会改变该文章的当前状态
+
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    // PendingReview实例会在调用approve时返回一个包裹在Box
+    // 内的Published结构体的新实例
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published{})
+    }
+
+    // 这里PendingReview也实现了content只不过这里是默认的实现方式
+}
+
+
+struct Published {}
+
+impl State for Published {
+    // 返回的都是本身，因为处于Publish的状态下的文章不应当本这些操作改变状态
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    // 同上注释
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
+#[test]
+fn exmaple(){
+    
+    // 主的流程逻辑
+    let mut post = Post::new();
+
+    // 此时出于草稿状态， 可以添加文字，到那时不能获取到文章的内容
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    // 此时是一个状态的转换，从草稿状态转换为审批状态， 此时已然无法获得内容
+    post.request_review();
+    assert_eq!("", post.content());
+
+    // 此时是一个状态的转换，从审批状态到发布状态，此时可以获得文章的内容
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    //-----------implement blog post -------------------------------
+
+}
