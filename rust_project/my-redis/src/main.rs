7729fdfbf8838e05c58aef62524268b99db4882f
DaviRain-Su <davirain.yin@gmail.com>
2020-10-28 15:04:34 +0800

    update shared state

--- rust_project/my-redis/src/main.rs
+++ rust_project/my-redis/src/main.rs
@@ -1,30 +1,37 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // The second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
         
+        let db = db.clone();
         // A new task is spawned for each inbound socket. The socket is 
-        // moved to the new task and processed there. 
+        // moved to the new task and processed there.
+        println!("Accepted"); 
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
    use mini_redis::Command::{self, Get, Set};
-   use std::collections::HashMap;
-
-   // A hashmap is used to store data 
-   let mut db = HashMap::new();
-
+   
    // Connection, provided by 'mini-redis', 
    // handles parsing frames the socket
    let mut connection = Connection::new(socket);
@@ -32,8 +39,8 @@ async fn process(socket: TcpStream) {
    // Use 'read_frame' to receive a command from the connection. 
    while let Some(frame) = connection.read_frame().await.unwrap() {
        let response = match Command::from_frame(frame).unwrap() {
-
            Set(cmd) => {
+               let mut db = db.lock().unwrap();
                // The  value is stored as 'Vec<u8>' 
                db.insert(cmd.key().to_string(), cmd.value().clone());
                Frame::Simple("OK".to_string())
@@ -42,6 +49,7 @@ async fn process(socket: TcpStream) {
                // 'Frame::Bulk' expects data to be of type 'Bytes'. 
                // This type will be covered later in the tutorial. 
                // For now, '&Vec<u8>' is converted to 'Bytes' using 'into()'
+               let db = db.lock().unwrap();
                if let Some(value) = db.get(cmd.key()) {
                    Frame::Bulk(value.clone().into())
                }else {
