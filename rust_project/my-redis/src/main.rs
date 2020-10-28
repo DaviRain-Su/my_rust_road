b99de29949ed4a98518e2525539af153b62e2231
DaviRain-Su <davirain.yin@gmail.com>
2020-10-28 12:06:13 +0800

    update process get set command

--- rust_project/my-redis/src/main.rs
+++ rust_project/my-redis/src/main.rs
@@ -17,15 +17,34 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
-    // The "Connection" lets us read/write redis **frames** instead 
-    // byte streams, The "Connection" type is defined by mini-redis. 
-    let mut connection = Connection::new(socket);
+   use mini_redis::Command::{self, Get, Set};
+   use std::collections::HashMap;
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+   // A hashmap is used to store data 
+   let mut db = HashMap::new();
 
-        // Respond with an error 
-        let response = Frame::Error("unimplemented".to_string());
-        connection.write_frame(&response).await.unwrap();
-    }
+   // Connection, provided by 'mini-redis', 
+   // handles parsing frames the socket
+   let mut connection = Connection::new(socket);
+
+   // Use 'read_frame' to receive a command from the connection. 
+   while let Some(frame) = connection.read_frame().await.unwrap() {
+       let response = match Command::from_frame(frame).unwrap() {
+           Set(cmd) => {
+               db.insert(cmd.key().to_string(), cmd.value().clone());
+               Frame::Simple("OK".to_string())
+           }
+           Get(cmd) => {
+               if let Some(value) = db.get(cmd.key()) {
+                   Frame::Bulk(value.clone())
+               }else {
+                   Frame::Null
+               }
+           }, 
+           cmd => panic!("unimplemented {:?}", cmd),
+       };
+
+       // Write the respinse to the client 
+       connection.write_frame(&response).await.unwrap();
+   }
 }
