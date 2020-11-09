ba9217b27f8ee2a79c6077c13a966091a46b0ac5
DaviRain-Su <davirain.yin@gmail.com>
2020-11-09 12:04:58 +0800

    modify handle_connection

---
+++ rust_project/hello/src/lib.rs
@@ -0,0 +1,106 @@
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+// struct Job;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+
+impl ThreadPool {
+    /// 创建线程池
+    ///
+    /// 线程池中线程的数量
+    ///
+    /// # Panics
+    ///
+    /// 'new' 函数会在size为0时触发panic
+    ///
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let mut workers = Vec::with_capacity(size);
+
+        let receiver = Arc::new(Mutex::new(receiver));
+        for id in 0..size {
+            // 创建线程并将它们存储至动态数组中
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+pub struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Self {
+        let thread = thread::spawn(move || {
+            // receiver;
+            loop {
+                let message = receiver.lock().unwrap().recv().unwrap();
+                match message {
+                    Message::NewJob(job) => {
+                        println!("Worker {} got a job; executing.", id);
+
+                        job();
+                    },
+                    Message::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+
+                        break;
+                    },
+                }
+            }
+        });
+
+        Self { id,
+            thread : Some(thread)
+        }
+    }
+}
