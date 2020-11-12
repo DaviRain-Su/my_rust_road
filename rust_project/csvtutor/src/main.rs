787f44d53ac7f7b9e6c3b5d9162efe9d75bf94c2
DaviRain-Su <davirain.yin@gmail.com>
2020-11-12 11:22:03 +0800

    reading csv step3

--- rust_project/csvtutor/src/main.rs
+++ rust_project/csvtutor/src/main.rs
@@ -2,7 +2,7 @@
 extern crate csv;
 
 // Import the standard library's I/O module so we can read from stdin.
-// use std::io;
+use std::io;
 use std::error::Error;
 use std::process;
 use std::ffi::OsString;
@@ -20,11 +20,28 @@ fn main() {
 
 fn run() -> Result<(), Box<dyn Error>> {
     let file_path = get_first_arg()?;
+
+    // let mut rdr =   csv::ReaderBuilder::new()
+    //     .has_headers(false)
+    //     .from_reader(io::stdin());
+    //
+    // {
+    //     // We nest this call in its own scope because of lifetimes.
+    //     let headers = rdr.headers()?;
+    //     println!("{:?}", headers);
+    // }
+    // let headers = rdr.headers()?.clone();
+    // println!("{:?}", headers);
     let mut rdr = csv::Reader::from_path(file_path)?;
+
     for result in rdr.records() {
         let record = result?;
         println!("{:?}", record);
     }
+    // We can ask for the headers at any time. There's no need to nest this
+    // call in its own scope because we never try to borrow the reader again.
+    // let headers = rdr.headers()?;
+    // println!("{:?}", headers);
     Ok(())
 }
 
