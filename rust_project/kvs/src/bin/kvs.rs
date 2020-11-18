ccdc3950dd0117990483bd0000d70a11c2eb86f2
DaviRain-Su <davirain.yin@gmail.com>
2020-11-18 17:50:21 +0800

    update command parse used by structopt

--- rust_project/kvs/src/bin/kvs.rs
+++ rust_project/kvs/src/bin/kvs.rs
@@ -1,50 +1,55 @@
-extern crate clap;
-use clap::{App, AppSettings, Arg, SubCommand};
 use std::process::exit;
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "kvs")]
+/// A key-value database
+struct Opts {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum  Command {
+    #[structopt(name = "set")]
+    /// Set the value of a string key to a string
+    Set {
+        key: String,
+        value: String,
+    },
+    #[structopt(name = "get")]
+    /// Get the string value of a given string key
+    Get {
+        key: String,
+    },
+    #[structopt(name = "rm")]
+    /// Remove a given key
+    Rm {
+        key: String,
+    }
+}
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set the value of a string key to a string")
-                .arg(Arg::with_name("KEY").help("A string key.").required(true))
-                .arg(
-                    Arg::with_name("VALUE")
-                        .help("The string value of the key")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("Get the string value of a given string key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("Remove a given key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
-        )
-        .get_matches();
+    let opts : Opts = Opts::from_args();
 
-    match matches.subcommand() {
-        ("set", Some(_matches)) => {
+    match opts.cmd {
+        Command::Set {
+            key: _key,
+            value: _value,
+        } => {
+            eprintln!("unimplemented");
+            exit(1);
+        },
+        Command::Get {
+            key: _key,
+        } => {
+            eprintln!("unimplemented");
+            exit(1);
+        },
+        Command::Rm {
+            key: _key,
+        } => {
             eprintln!("unimplemented");
             exit(1);
         }
-        ("get", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
-        }
-        ("rm", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
-        }
-        _ => unreachable!(),
     }
 }
