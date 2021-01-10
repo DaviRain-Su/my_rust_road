2a4a671793814230ccda1110c4c31e9f8a667d6e
DaviRain-Su <davirain.yin@gmail.com>
2021-01-10 15:28:01 +0800

    update ...

--- projects_rust/hecto/src/main.rs
+++ projects_rust/hecto/src/main.rs
@@ -1,13 +1,25 @@
 use std::io::{self, Read, stdout};
 use termion::raw::IntoRawMode;
 
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let bytes = c as u8;
+    bytes & 0b0001_1111
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let c  = b.unwrap() as char;
-        println!("{}", c);
-        if c == 'q' {
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+        // if c == 'q' {
+        if b == to_ctrl_byte('q') {
             break;
         }
     }
