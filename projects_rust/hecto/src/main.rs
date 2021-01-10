7b2662ce821ae41c1dbc831da09364eaf3c9bfae
DaviRain-Su <davirain.yin@gmail.com>
2021-01-10 14:52:48 +0800

    update ...

--- projects_rust/hecto/src/main.rs
+++ projects_rust/hecto/src/main.rs
@@ -1,6 +1,9 @@
-use std::io::{self, Read};
+use std::io::{self, Read, stdout};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes() {
         let c  = b.unwrap() as char;
         println!("{}", c);
