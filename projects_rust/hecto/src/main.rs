2021eb49d523ceb99a4662a8c589b3a6730818f5
DaviRain-Su <davirain.yin@gmail.com>
2021-01-10 14:30:16 +0800

    update ...

--- projects_rust/hecto/src/main.rs
+++ projects_rust/hecto/src/main.rs
@@ -1,4 +1,8 @@
+use std::io::{self, Read};
+
 fn main() {
-    
-    println!("Hello, world!");
+    for b in io::stdin().bytes() {
+        let c  = b.unwrap() as char;
+        println!("{}", c);
+    }
 }
