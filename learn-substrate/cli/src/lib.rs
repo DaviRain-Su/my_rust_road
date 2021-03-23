1ebe0ea34ba3a4e336deb1304b4d0d7c3fd5645c
DaviRain-Su <davirain.yin@gamil.com>
2021-03-23 15:15:37 +0800

    use yaml

--- learn-substrate/cli/src/lib.rs
+++ learn-substrate/cli/src/lib.rs
@@ -1,45 +1,44 @@
-use clap::{Arg, App, SubCommand};
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate log;
 
 pub fn main() {
-	let matches = App::new("Polkadot")
-		.arg(Arg::with_name("log")
-			.short("l")
-			.value_name("LOG")
-			.help("Sets logging.")
-			.takes_value(true))
-		.subcommand(SubCommand::with_name("collator"))
-		.subcommand(SubCommand::with_name("validator"))
-		.get_matches();
+    // let yaml = load_yaml!("./cli.yml");
+    // let matches = clap::App::from_yaml(yaml).get_matches();
+    let yaml = load_yaml!("./cli.yml");
+    let matches = clap::App::from_yaml(yaml).get_matches();
 
-	let log_pattern = matches.value_of("log").unwrap_or("");
-	init_logger(log_pattern);
+    let log_pattern = matches.value_of("log").unwrap_or("");
+    init_logger(log_pattern);
 
-	if let Some(_) = matches.subcommand_matches("collator") {
-		println!("Running collator.");
-		return;
-	}
+    if let Some(_) = matches.subcommand_matches("collator") {
+        info!("Running collator.");
+        return;
+    }
 
-	if let Some(_) = matches.subcommand_matches("validator") {
-		println!("Running collator.");
-		return;
-	}
+    if let Some(_) = matches.subcommand_matches("validator") {
+        info!("Running collator.");
+        return;
+    }
+
+    println!("No command given.\n");
+    let _ = clap::App::from_yaml(yaml).print_long_help();
 }
 
-
 fn init_logger(pattern: &str) {
-	let mut builder = env_logger::Builder::new();
-	// Disable info logging by default for some modules:
-	builder.filter(Some("hyper"), log::LevelFilter::Warn);
-	// Enable info for others.
-	builder.filter(None, log::LevelFilter::Info);
+    let mut builder = env_logger::Builder::new();
+    // Disable info logging by default for some modules:
+    builder.filter(Some("hyper"), log::LevelFilter::Warn);
+    // Enable info for others.
+    builder.filter(None, log::LevelFilter::Info);
 
-	if let Ok(lvl) = std::env::var("RUST_LOG") {
-		builder.parse_env(&lvl);
-	}
+    if let Ok(lvl) = std::env::var("RUST_LOG") {
+        builder.parse_env(&lvl);
+    }
 
-	builder.parse_filters(pattern);
+    builder.parse_filters(pattern);
 
-
-	// builder.init().expect("Logger initialized only once.");
-	builder.init();
+    // builder.init().expect("Logger initialized only once.");
+    builder.init();
 }
